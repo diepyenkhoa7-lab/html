8abd6da359189029cc17c5c1e8c4e7d752a42ce5
khoa <diepyenkhoa7@gmail.com>
2025-10-29 16:34:30 +0700

    modified:   src/supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://kxkcumlkmwjkgkrjzqxo.supabase.co";
 const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXRlaXh1enVsd21ydHhzZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYyMTQsImV4cCI6MjA3MTc5MjIxNH0.w7PLqjLGj4hNNGDh81NwDodEUCCqVSVm_PL0FpYWif8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4a2N1bWxrbXdqa2drcmp6cXhvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTcyNTIyNiwiZXhwIjoyMDc3MzAxMjI2fQ.S6YGoLLOs3dP868ilSB3SS_XNb6uLmbLCJJ7JPsbJJw";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
